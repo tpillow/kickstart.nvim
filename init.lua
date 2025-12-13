2499478cd5710a43f65edfa110bd8fd5359f8858
codenmore <jackofall55@gmail.com>
2025-12-12 22:04:27 -0500

    tab settings

--- init.lua
+++ init.lua
@@ -121,6 +121,11 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Convert tabs to spaces
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Save undo history
 vim.o.undofile = true
 
